5db624ca942fa64142b53af15a7376bc4ccb7153
Kashif Ansari <kansari@umd.edu>
2024-10-23 21:48:43 -0400

    merging

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/test
+# Build directory: /home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/build/test/cpp-test[1]_include.cmake")
