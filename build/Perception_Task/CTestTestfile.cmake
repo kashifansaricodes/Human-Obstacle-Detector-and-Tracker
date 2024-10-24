4819a68e03e171fb06b0fb9f7037b4c61cb319a3
Kashif Ansari <155030670+kashifansaricodes@users.noreply.github.com>
2024-10-23 21:42:39 -0400

    Delete build directory

--- build/Perception_Task/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/Perception_Task
-# Build directory: /home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/build/Perception_Task
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
