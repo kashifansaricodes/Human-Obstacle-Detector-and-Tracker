80a9385c55e710c895b176472a0ff36f2ef8cdf0
Kashif Ansari <kansari@umd.edu>
2024-10-23 22:13:01 -0400

    pushing upadte

---
+++ build/app/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/app
+# Build directory: /home/kashif/SDR/week6/Human-Obstacle-Detector-and-Tracker/build/app
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
