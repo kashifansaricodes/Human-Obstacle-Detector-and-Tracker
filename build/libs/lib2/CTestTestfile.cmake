a8c9579d541f572b5c1492f6c87d0b1eea95aa26
Abhishek260101 <avhad.abhishek005@gmail.com>
2024-10-28 18:41:14 -0400

    Remove build folder and add it to gitignore

--- build/libs/lib2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abhishek/Human-Obstacle-Detector-and-Tracker/libs/lib2
-# Build directory: /home/abhishek/Human-Obstacle-Detector-and-Tracker/build/libs/lib2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abhishek/Human-Obstacle-Detector-and-Tracker/test
-# Build directory: /home/abhishek/Human-Obstacle-Detector-and-Tracker/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/abhishek/Human-Obstacle-Detector-and-Tracker/build/test/cpp-test[1]_include.cmake")
